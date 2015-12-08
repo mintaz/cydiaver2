e7c5844a86f0b11e95a96b9c284cec6d050856a6
ThoaiPham <thanlua199@yahoo.com.vn>
2015-12-08 22:59:14 +0700

    Fixed #6
    sửa lỗi màu cọ

--- k20jwhiteboard-master/src/jWhiteBoard/JWhiteBoard.java
+++ k20jwhiteboard-master/src/jWhiteBoard/JWhiteBoard.java
@@ -36,7 +36,7 @@ public class JWhiteBoard extends ReceiverAdapter implements ActionListener, Chan
     private JButton                clearButton, leaveButton;
     private final Random           random=new Random(System.currentTimeMillis());
     private final Font             defaultFont=new Font("Helvetica",Font.PLAIN,12);
-    private final Color            drawColor=Color.black;
+    private final Color            drawColor=selectColor(); //Fixed : only 1 color brush
     private static final Color     backgroundColor=Color.white;  //Fixed: background color
     boolean                        noChannel=false;
     boolean                        jmx;
@@ -243,7 +243,7 @@ public class JWhiteBoard extends ReceiverAdapter implements ActionListener, Chan
         int red=Math.abs(random.nextInt()) % 255;
         int green=Math.abs(random.nextInt()) % 255;
         int blue=Math.abs(random.nextInt()) % 255;
-        return new Color(red, blue, blue);
+        return new Color(red, green, blue); //Fixed: rgb color
     }
 
 
